014b82596dc89260b73289719430bba6179cef28
Shruti Shaw <shrutishaw750@gmail.com>
2024-10-02 10:22:02 +0530

    Initial commit

---
+++ Quiz/src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        questions: "Which device is required for the Internet Cnnection ?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans:1,
+    },
+    {
+        questions: "Which protocol is used to transfer files over the Internet?",
+        option1: "FTP",
+        option2: "HTTP",
+        option3: "SMTP",
+        option4: "POP3",
+        ans: 1
+    },
+    {
+        questions: "Which device distributes network traffic across multiple devices?",
+        option1: "Switch",
+        option2: "Modem",
+        option3: "Printer",
+        option4: "Scanner",
+        ans: 1
+    },
+    {
+        questions: "Which technology is used for wireless communication over short distances?",
+        option1: "Wi-Fi",
+        option2: "Bluetooth",
+        option3: "Ethernet",
+        option4: "Infrared",
+        ans: 2
+    },
+    {
+        questions: "Which IP address version provides a larger address space?",
+        option1: "IPv2",
+        option2: "IPv4",
+        option3: "IPv6",
+        option4: "IPv8",
+        ans: 3
+    },   
+];
+
